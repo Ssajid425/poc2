89610449fbe713314ad25766be7db0d08e997983
sajid <sajid04shaikh@gmail.com>
2023-05-25 23:07:42 +0530

    first commit

--- src/Components/stepperData/step1Form.js
+++ src/Components/stepperData/step1Form.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addFinancialInfo } from "../../store";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
+
+import {
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@mui/material'
 
 const Tag = styled(Stack)(({ theme }) => ({
   width: "147px",
@@ -14,98 +25,118 @@ const Tag = styled(Stack)(({ theme }) => ({
   alignItems: "center",
   padding: "2px 8px",
   gap: "8px",
-  color: "#B4C5D7",
-  margin: "auto"
+  color: "#283545",
+  // margin: "auto"
 }));
+const Text = styled('h4')(({ theme }) => ({
+    alignItems: 'center',
+    fontSize: '20px',
+    lineHeight: "24px",
+    display: 'flex',
+    flexDirection: 'column',
+    letterSpacing: "0.02em",
+    color: "#283545",
+    width: "448px",
+    paddingRight: '100px'
+  }));
 
-const FormContainer = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginTop: '5px',
-  height: '700px', // Set the desired height for the scrollable container
-  overflowY: 'auto'
-});
-
-const SelectWrapper = styled('div')({
-  boxSizing: 'border-box',
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  padding: '12px 16px',
-  gap: '10px',
-  width: '644px',
-  height: '48px',
-  background: '#FFFFFF',
-  borderWidth: '1px 1px 2px 1px',
-  borderStyle: 'solid',
-  borderColor: '#1353A1',
-  borderRadius: '4px',
-});
-
-const Dropdown = styled('select')({
-  display:"flex",
-  flexDirection:"coloumn",
-  alignItems:"center",
-  gap:"16px",
-  width:"644px",
   
-});
+
 
 export default function Step1Form() {
-  const dispatch = useDispatch();
-  const formData = require('../../jsonData/financialInfo.json');
 
-  const [formValues, setFormValues] = useState({});
 
-  const handleInputChange = (event, cardKey) => {
-    const { name, value, options, multiple } = event.target;
-    const selectedOptions = Array.from(options)
-      .filter((option) => option.selected)
-      .map((option) => option.value);
+  const dispatch= useDispatch();
 
-    setFormValues((prevState) => ({
-      ...prevState,
-      [name]: multiple ? selectedOptions : value
-    }));
-  };
+  const jsonFinancialData = require('../../jsonData/financialInfo.json');
+  const dataString=JSON.stringify(jsonFinancialData,null,2);
+  const data=JSON.parse(dataString);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
 
-    dispatch(addFinancialInfo(formValues));
-  };
+
+//Below function maintains the dropdown to select the values
+
+const [selectedOption, setSelectedOption] = useState(Array(data.cards.length).fill(''));
+
+const handleDropdownChange = (event, index) => {
+  const updatedOptions = [...selectedOption];
+  updatedOptions[index] = event.target.value;
+  setSelectedOption(updatedOptions);
+  dispatch(addFinancialInfo(event.target.value));
+};
+
 
   return (
     <>
-      <Tag>
-        <Chip label="Financial Information" />
-      </Tag>
-      <FormContainer>
-        <form onSubmit={handleSubmit}>
-          {/* Render the form fields based on the JSON data */}
-          {/* You can map over the 'cards' array and render each card dynamically */}
-          {formData.cards.map((card) => (
-            <div key={card.cardKey}>
-              <h3>{card.header}</h3>
-              {/* Render the dropdown menu */}
-              <SelectWrapper>
-                <Dropdown
-                  name={card.header}
-                  onChange={(e) => handleInputChange(e, card.cardKey)}
-                  multiple={card.isMultiSelect}
-                >
-                  {card.options.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </Dropdown>
-              </SelectWrapper>
-            </div>
-          ))}
-        </form>
-      </FormContainer>
+      
+                    <div style={{display: "flex",
+                     alignItem:"center",
+                     justifyContent:"center", 
+                     paddingTop:"40px", 
+                     paddingBottom: "80px",
+                     backgroundColor: "#F5F5F5"}}>
+                        <form>
+                        <Tag style={{display:"flex-start"}}>
+                        <Chip label="Financial Information" />
+                      </Tag>
+                                  <h4 style={{ fontSize: '20px',paddingLeft:"10px"}}>Financial datails</h4>  
+                                  <p style={{width: "644px",
+                                            height: "48px",
+                                            fontWeight: "400",
+                                            fontSize: "16px",
+                                            lineHeight: "24px",
+                                            paddingLeft:"10px",
+                                            marginTop: '-20px',
+                                            color: "#5B6A88",
+                                            }}>
+                                    Understanding your financial context helps us assess whether this account is suitable for you.
+                                    </p> 
+                                    <Box
+                                        sx={{
+                                            width: "644px",
+                                            height: "64px",
+                                            backgroundColor:  'rgba(180, 197, 215, 0.15)',
+                                            marginTop:"20px",
+                                            marginBottom:"10px",
+                                            marginLeft:"10px"
+                                        }}>
+                                        <Typography variant="body1" sx={{fontSize:14,fontWeight: 400,color: "#5B6A88",letterSpacing: "0.5px",paddingLeft:5,paddingTop:3}}>
+                                        <InfoOutlinedIcon sx={{ fontSize: 15,color: "#5B6A88",}} /> Please complete this section carefully.
+                                        </Typography> </Box>     
+                            {data.cards.map((c,index)=>{
+                                return(
+                                  <div>
+                                      <p sx= {{paddingLeft:30}}>{c.header}</p>
+
+                                  <FormControl  sx={{minWidth: "644px", margin: "10px", borderColor: '#1353A1',}}>
+                                    <InputLabel >{c.header}</InputLabel>
+                                    <Select
+                                      
+                                      sx={{display:"flex",boxSizing:"border-box",flexDirection:"row"}}
+                                      labelId="demo-simple-select-label"
+                                      varient='filled'
+                                      key={index}
+                                      value={selectedOption[index]}
+                                      onChange={(event) => handleDropdownChange(event, index)}
+
+                                      label="Select an option"
+                                    >
+                                      {c.options.map((option) => (
+                                        <MenuItem id="item" key={option.label} value={option.label}>
+                                          {option.label}
+                                        </MenuItem>
+                                      ))}
+                                    </Select>
+                                  </FormControl>
+                                  </div>
+                                )}
+                            )}
+                              
+                            
+                          </form>
+                    </div>
+            
+      
     </>
   );
 }
--- src/Components/stepperData/step2Form.js
+++ src/Components/stepperData/step2Form.js
@@ -1,7 +1,135 @@
-import react from "react";
+import React, { useEffect,useState } from 'react';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import { useDispatch } from "react-redux";
+import { addTradingInfo } from "../../store";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
-export default function Step2Form(){
-return(
-    <h5>This is form no2</h5>
-)
+import {
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@mui/material'
+
+const Tag = styled(Stack)(({ theme }) => ({
+  width: "147px",
+  height: "100px",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "2px 8px",
+  gap: "8px",
+  color: "#283545",
+  // margin: "auto"
+}));
+const Text = styled('h4')(({ theme }) => ({
+    alignItems: 'center',
+    fontSize: '20px',
+    lineHeight: "24px",
+    display: 'flex',
+    flexDirection: 'column',
+    letterSpacing: "0.02em",
+    color: "#283545",
+    width: "448px",
+    paddingRight: '100px'
+  }));
+  
+
+
+export default function Step1Form() {
+
+
+  const dispatch= useDispatch();
+
+  const jsonFinancialData = require('../../jsonData/tradingInfo.json');
+  const dataString=JSON.stringify(jsonFinancialData,null,2);
+  const data=JSON.parse(dataString);
+
+
+
+//Below function maintains the dropdown to select the values
+
+      const [selectedOption, setSelectedOption] = useState([]);
+      const handleDropdownChange = (event) => {
+        setSelectedOption(...selectedOption,event.target.value);
+        dispatch(addTradingInfo(event.target.value));
+      };
+     
+
+  return (
+    <>
+      
+                    <div style={{display: "flex",
+                     alignItem:"center",
+                     justifyContent:"center", 
+                     paddingTop:"40px", 
+                     paddingBottom: "80px",
+                     backgroundColor: "#F5F5F5"}}>
+                        
+                        <form>
+                        <Tag style={{display:"flex-start"}}>
+                        <Chip label="Trading Information" />
+                      </Tag>
+                                  <h4 style={{ fontSize: '20px',paddingLeft:"10px"}}>Trading datails</h4> 
+                                  <p style={{width: "644px",
+                                            height: "48px",
+                                            fontWeight: "400",
+                                            fontSize: "16px",
+                                            lineHeight: "24px",
+                                            paddingLeft:"10px",
+                                            marginTop: '-20px',
+                                            color: "#5B6A88",
+                                            }}>
+                                   Understanding your level of trading experience helps us determine if an FXCM account is right for you. 
+                                    </p> 
+                                    <Box
+                                        sx={{
+                                            width: "644px",
+                                            height: "64px",
+                                            backgroundColor:  'rgba(180, 197, 215, 0.15)',
+                                            marginTop:"20px",
+                                            marginBottom:"10px",
+                                            marginLeft:"10px"
+                                        }}>
+                                        <Typography variant="body1" sx={{fontSize:14,fontWeight: 400,color: "#5B6A88",letterSpacing: "0.5px",paddingLeft:5,paddingTop:3}}>
+                                        <InfoOutlinedIcon sx={{ fontSize: 15,color: "#5B6A88",}} /> Please complete this section carefully.
+                                        </Typography> </Box>     
+                            {data.cards.map((c,index)=>{
+                                return(
+                                  <div>
+                                     <h3>{c.header}</h3>
+                                  <FormControl  sx={{minWidth: "644px", margin: "10px"}}>
+                                    <InputLabel >{c.header}</InputLabel>
+                                    <Select
+                                      labelId="demo-simple-select-label"
+                                      varient='filled'
+                                      key={index}
+                                      value={selectedOption[index]}
+                                      onChange={handleDropdownChange}
+                                      label="Select an option"
+                                      sx={{maxHeight:"48px"}}
+                                    >
+                                      {c.options.map((option) => (
+                                        <MenuItem id="item" key={option.label} value={option.label}>
+                                          {option.label}
+                                        </MenuItem>
+                                      ))}
+                                    </Select>
+                                  </FormControl>
+                                  </div>
+                                )}
+                            )}
+                              
+                            
+                          </form>
+                    </div>
+            
+      
+    </>
+  );
 }
